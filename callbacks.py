cc66d50046cca6b00759192128c61a7ea23280b3
Aaron Duke <dukuru@gmail.com>
2019-03-26 17:48:55 -0700

    Cleanup python scripts

--- callbacks.py
+++ callbacks.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-import time
-import os
-import argparse   
-import sys
+import argparse
 import json
+import os
 import subprocess
+import sys
+import time
+
 
 class Logger(object):
     def __init__(self, filename="/var/log/edmrds.log"):
@@ -16,33 +17,33 @@ class Logger(object):
         self.terminal.write(message)
         self.log.write(message)
 
+
 sys.stdout = Logger("/home/fpp/media/logs/edmrds.log")
 
 parser = argparse.ArgumentParser(description='RDS Setting Application')
-parser.add_argument('-t','--type', help='Input station name (8 characters max)', required=False)
-parser.add_argument('-l','--list',help='Song name',action='store_true')
-parser.add_argument('-d','--data',help='Song name')
+parser.add_argument(
+    '-t',
+    '--type',
+    help='Input station name (8 characters max)',
+    required=False)
+parser.add_argument('-l', '--list', help='Song name', action='store_true')
+parser.add_argument('-d', '--data', help='Song name')
 args = parser.parse_args()
 
 if args.list:
-   #Tell the plugin that we should be registered for media
-   print("media")
+    # Tell the plugin that we should be registered for media
+    print("media")
 
 if args.type:
-   # Look for our type of plugin, doubt this is even needed at all
-   mytypearg = args.type
-   print("My type args: %s" %mytypearg)
+    # Callback should be invoked with --type media
+    print("type: %s" % args.type)
 
 if args.data:
-   #get the json string from FPP
-   mydataarg = args.data
-   print("My data args: %s" %mydataarg)
-   data = json.loads(mydataarg)
-   title = data['title'] 
-   print("Title: %s" %title)
-   os.chdir("/home/fpp/media/plugins/edmrds/")
-   directory = os.getcwd()
-   print("Directory: %s" %directory)
-   #from subprocess import call
-   #call(["rds-song.py", "-s %title"])
-   subprocess.call(['/home/fpp/media/plugins/edmrds/rds-song.py', '-s', title])
+    # Get the json string from FPP
+    print("data: %s" % args.data)
+    data = json.loads(args.data)
+    title = data['title']
+    print("title: %s" % title)
+    plugin_dir = os.path.dirname(__file__)
+    subprocess.call(
+        [os.path.join(plugin_dir, 'rds-song.py'), '-s', title], env={"PYTHONPATH": plugin_dir})
