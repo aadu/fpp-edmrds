2e2ec49ceb82fec619ad59573e102be9af203bdb
Aaron Duke <dukuru@gmail.com>
2019-03-26 17:50:26 -0700

    Add help

---
+++ help/fpp-edmrds.php
@@ -0,0 +1,5 @@
+<?php
+echo "<br><br><a href='https://github.com/FalconChristmas/fpp-edmrds' target='_blank'>Please click here to go to the help page</a>";
+echo "<br><br><a href='https://falconchristmas.com/forum/index.php/topic,8329.msg87563.html#msg87563' target='_blank'>Helpful info although somewhat outdated</a>";
+echo "<br><br><a href='https://falconchristmas.com/forum/index.php/topic,10542.0.html' target='_blank'>Additional discussion</a>";
+?>
